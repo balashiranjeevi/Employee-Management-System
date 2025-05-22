88110d6c33808f733bd139b4dcd7b36f2a87517c
BALA SHIRANJEEVI <balashiranjeevi97@gmail.com>
2025-05-22 16:40:50 +0530

    Implement login functionality with admin authentication and database connection

--- EMS/src/components/Login.jsx
+++ EMS/src/components/Login.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import './style.css';
-
+import axios from "axios";
 
 const Login = () => {
+  const [values, setValues] = useState({
+    email: "",
+    password: ""
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post("http://localhost:3000/auth/adminlogin", values)
+      .then(result => console.log(result))
+      .catch(err => console.log(err));
+  };
+
   return (
     <div className="d-flex flex-column justify-content-center align-items-center vh-100 loginPage">
       <div className="p3 rounded w-25 border loginForm">
         <h2>Login page</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
             <label htmlFor="email">
               <strong>Email:</strong>
@@ -18,6 +30,7 @@ const Login = () => {
               name="email"
               autoComplete="off"
               placeholder="Enter E-mail"
+              onChange={(e) => setValues({ ...values, email: e.target.value })}
               className="form-control rounded-0"
               required
             />
@@ -31,6 +44,7 @@ const Login = () => {
               id="password"
               name="password"
               placeholder="Enter Password"
+              onChange={(e) => setValues({ ...values, password: e.target.value })}
               className="form-control rounded-0"
               required
             />
