96b4fd062af8fed96270eb847fa2cf4ed81e60ce
BALA SHIRANJEEVI <balashiranjeevi97@gmail.com>
2025-05-23 16:26:09 +0530

    Add routing and components for employee management system; enhance dashboard layout

--- EMS/src/App.jsx
+++ EMS/src/App.jsx
@@ -1,18 +1,27 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'; 
-import Login from './components/Login'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Dashboard from './components/Dashboard';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Login from "./components/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Home from "./components/Home";
+import Employee from "./components/Employee";
+import Category from "./components/Category";
+import Profile from "./components/Profile";
 
 function App() {
-    return (
+  return (
     <BrowserRouter>
-    <Routes>
-      <Route path='adminlogin' element = {<Login />}></Route>
-      <Route path='dashboard' element = {<Dashboard />}></Route>
-    </Routes>
+      <Routes>
+        <Route path="adminlogin" element={<Login />}></Route>
+        <Route path="dashboard" element={<Dashboard />}>
+          <Route path="" element={<Home />}></Route>
+          <Route path="/dashboard/employee" element={<Employee />}></Route>
+          <Route path="/dashboard/category" element={<Category />}></Route>
+          <Route path="/dashboard/profile" element={<Profile />}></Route>
+        </Route>
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- EMS/src/components/Login.jsx
+++ EMS/src/components/Login.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
-import './style.css';
+import "./style.css";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const [values, setValues] = useState({
     email: "",
-    password: ""
+    password: "",
   });
 
   const [error, setError] = useState(null);
@@ -15,25 +15,22 @@ const Login = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post("http://localhost:3000/auth/adminlogin", values)
-      .then(result => {
-        if (result.data.loginStatus){
+    axios
+      .post("http://localhost:3000/auth/adminlogin", values)
+      .then((result) => {
+        if (result.data.loginStatus) {
           navigate("/dashboard");
-        }else{
+        } else {
           setError(result.data.Error);
         }
-        
-
       })
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err));
   };
 
   return (
     <div className="d-flex flex-column justify-content-center align-items-center vh-100 loginPage">
       <div className="p3 rounded w-25 border loginForm">
-        <div className="text-warning">
-          {error && error}
-        </div>
+        <div className="text-warning">{error && error}</div>
         <h2>Login page</h2>
         <form onSubmit={handleSubmit}>
           <div>
@@ -60,7 +57,9 @@ const Login = () => {
               id="password"
               name="password"
               placeholder="Enter Password"
-              onChange={(e) => setValues({ ...values, password: e.target.value })}
+              onChange={(e) =>
+                setValues({ ...values, password: e.target.value })
+              }
               className="form-control rounded-0"
               required
             />
