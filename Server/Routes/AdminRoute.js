670890908a01469c20dba00989fc6daf0f141834
BALA SHIRANJEEVI <balashiranjeevi97@gmail.com>
2025-05-29 15:16:06 +0530

    Add logout functionality to Dashboard and server route

--- Server/Routes/AdminRoute.js
+++ Server/Routes/AdminRoute.js
@@ -184,4 +184,8 @@ router.get("/admin_records", (req, res) => {
   });
 });
 
+router.get("/logout", (req, res) => {
+  res.clearCookie("token");
+  return res.json({ Status: true });
+});
 export { router as adminRouter };
